1dd6f518a091ccd072ebd455969b79916684f8d8
Vanshaj Gupta <dpsven12130@gmail.com>
2023-03-06 20:57:23 +0530

    Profile Page Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:myflutterproject/pages/profile_page.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  // Handles Status and Nav bar styling/theme
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.transparent,
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.dark,
+  ));
+}
 
+class MyApp extends StatefulWidget {
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        title: 'Login Page ',
-        home: Scaffold(
-          body: Text(
-            "hey Flutter",
-            textAlign: TextAlign.center,
+    return MaterialApp(
+      title: 'User Profile',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primaryColor: Colors.black,
+          fontFamily: 'Roboto',
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.black,
+              shadowColor: Colors.grey,
+              elevation: 20,
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(0.0),
+                ),
+              ),
+            ),
           ),
-        ));
+          inputDecorationTheme: InputDecorationTheme(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(0.0),
+            ),
+          ),
+          textButtonTheme: TextButtonThemeData(
+            style: TextButton.styleFrom(
+              alignment: Alignment.centerLeft,
+              primary: Colors.black,
+            ),
+          )),
+      home: const ProfilePage(),
+    );
   }
 }
---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,174 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:myflutterproject/pages/edit_description.dart';
+import 'package:myflutterproject/pages/edit_email.dart';
+import 'package:myflutterproject/pages/edit_image.dart';
+import 'package:myflutterproject/pages/edit_name.dart';
+import 'package:myflutterproject/pages/edit_phone.dart';
+import '../user/user.dart';
+import '../widgets/display_image_widget.dart';
+import '../user/user_data.dart';
+
+// This class handles the Page to dispaly the user's info on the "Edit Profile" Screen
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    final user = UserData.myUser;
+
+    return Scaffold(
+      body: Column(
+        children: [
+          AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            toolbarHeight: 10,
+          ),
+          const Center(
+              child: Padding(
+                  padding: EdgeInsets.only(bottom: 20),
+                  child: Text(
+                    'Edit Profile',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w700,
+                      color: Color.fromRGBO(64, 105, 225, 1),
+                    ),
+                  ))),
+          InkWell(
+              onTap: () {
+                navigateSecondPage(const EditImagePage());
+              },
+              child: DisplayImage(
+                imagePath: user.image,
+                onPressed: () {},
+              )),
+          buildUserInfoDisplay(user.name, 'Name', const EditNameFormPage()),
+          buildUserInfoDisplay(user.phone, 'Phone', const EditPhoneFormPage()),
+          buildUserInfoDisplay(user.email, 'Email', const EditEmailFormPage()),
+          Expanded(
+            // ignore: sort_child_properties_last
+            child: buildAbout(user),
+            flex: 4,
+          )
+        ],
+      ),
+    );
+  }
+
+  // Widget builds the display item with the proper formatting to display the user's info
+  Widget buildUserInfoDisplay(String getValue, String title, Widget editPage) =>
+      Padding(
+          padding: const EdgeInsets.only(bottom: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.grey,
+                ),
+              ),
+              const SizedBox(
+                height: 1,
+              ),
+              Container(
+                  width: 350,
+                  height: 40,
+                  decoration: const BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(
+                    color: Colors.grey,
+                    width: 1,
+                  ))),
+                  child: Row(children: [
+                    Expanded(
+                        child: TextButton(
+                            onPressed: () {
+                              navigateSecondPage(editPage);
+                            },
+                            child: Text(
+                              getValue,
+                              style: const TextStyle(fontSize: 16, height: 1.4),
+                            ))),
+                    const Icon(
+                      Icons.keyboard_arrow_right,
+                      color: Colors.grey,
+                      size: 40.0,
+                    )
+                  ]))
+            ],
+          ));
+
+  // Widget builds the About Me Section
+  Widget buildAbout(User user) => Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Tell Us About Yourself',
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+              color: Colors.grey,
+            ),
+          ),
+          const SizedBox(height: 1),
+          Container(
+              width: 350,
+              height: 200,
+              decoration: const BoxDecoration(
+                  border: Border(
+                      bottom: BorderSide(
+                color: Colors.grey,
+                width: 1,
+              ))),
+              child: Row(children: [
+                Expanded(
+                    child: TextButton(
+                        onPressed: () {
+                          navigateSecondPage(const EditDescriptionFormPage());
+                        },
+                        child: Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 10, 10, 10),
+                            child: Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  user.aboutMeDescription,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    height: 1.4,
+                                  ),
+                                ))))),
+                const Icon(
+                  Icons.keyboard_arrow_right,
+                  color: Colors.grey,
+                  size: 40.0,
+                )
+              ]))
+        ],
+      ));
+
+  // Refrshes the Page after updating user info.
+  FutureOr onGoBack(dynamic value) {
+    setState(() {});
+  }
+
+  // Handles navigation and prompts refresh.
+  void navigateSecondPage(Widget editForm) {
+    Route route = MaterialPageRoute(builder: (context) => editForm);
+    Navigator.push(context, route).then(onGoBack);
+  }
+}
